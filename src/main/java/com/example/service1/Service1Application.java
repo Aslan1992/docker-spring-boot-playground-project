04a080ae77b5cddd96b18729c95dbefc4d12d1e8
Aslan_Almaniyazov <aslan_almaniyazov@epam.com>
2020-12-25 10:18:39 +0600

    test branch commit message - 1

--- src/main/java/com/example/service1/Service1Application.java
+++ src/main/java/com/example/service1/Service1Application.java
@@ -3,6 +3,8 @@ package com.example.service1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //test branch commit message
+//test branch commit message 2
+//test branch commit message 3
 @SpringBootApplication
 public class Service1Application {
 

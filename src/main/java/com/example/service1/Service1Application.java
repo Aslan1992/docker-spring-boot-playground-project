8f30bc2476c9e05e9b32f90e6eb8f1dc8b0e21a7
Aslan_Almaniyazov <aslan_almaniyazov@epam.com>
2020-12-25 10:42:11 +0600

    commit1

--- src/main/java/com/example/service1/Service1Application.java
+++ src/main/java/com/example/service1/Service1Application.java
@@ -3,6 +3,7 @@ package com.example.service1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//commit1
 @SpringBootApplication
 public class Service1Application {
 
